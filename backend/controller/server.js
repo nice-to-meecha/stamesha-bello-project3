f899fda5cce5ba27c567ca6af2bb6178effaf9c9
Stamesha Bello <stameshabello@gmail.com>
2023-04-28 21:35:32 -0700

    Serving built HTML/CSS from server.js

--- backend/controller/server.js
+++ backend/controller/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+
 const users = require("./user");
 const statusUpdates = require("./statusUpdate");
 const images = require("./image");
@@ -17,6 +19,13 @@ app.use("/api/statusUpdates/", statusUpdates);
 app.use("/api/images", images);
 
 mongoose.connect(process.env.MONGO_DB_ENDPOINT);
+
+let frontend_dir = path.join(__dirname, '..', '..', 'frontend', 'dist');
+
+app.use(express.static(frontend_dir));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(frontend_dir, "index.html"));
+});
 
 app.listen(process.env.port || 8000, () => {
     console.log("Starting project3 server!");
